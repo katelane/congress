0afa333b3d5679062164c8dd408cd90c04d672af
Kate Lane <katelane@gmail.com>
2015-01-21 23:14:24 -0700

    initial API pass pt 2 - OpenStruct (temporarily?) stripped out

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -4,18 +4,20 @@ class Legislator
   end
 
   def self.all
-    service.legislators.map { |legislator| _build_object(legislator) }
+    service.legislators
+    # .map { |legislator| _build_object(legislator) }
   end
 
-  def self.find(id)
-    _build_object(service.legislator(id))
+  def self.find(bioguide_id)
+    # _build_object(service.legislator(id))
+    service.legislator(bioguide_id)
   end
 
-  private
-
-  def self._build_object(data)
-    # OpenStruct.new(data)
-    data.map { |hash| OpenStruct.new(hash) } 
-  end
+  # private
+  #
+  # def self._build_object(data)
+  #   # OpenStruct.new(data)
+  #   data.map { |hash| OpenStruct.new(hash) }
+  # end
 
 end
