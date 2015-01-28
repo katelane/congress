1a4720370f7348a9c0b3899efd5337e455817f58
Kate Lane <katelane@gmail.com>
2015-01-28 10:18:51 -0700

    style tweaks, search working, logout added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = user.id
-    redirect_to root_path, notice: "You have been logged in with Twitter."
+    redirect_to root_path
   end
 
   def destroy
     session.clear
-    redirect_to root_path, notice: "You've been succesfully signed out."
+    redirect_to root_path
   end
 
   def failure
